1c9d24d2009dc8f2742d69207c44dd524f7e3433
Alex.CMC <ntduc6@cmcglobal.vn>
2022-11-28 14:00:14 +0700

    done excersies 1.5

--- exercise_1/src/App.js
+++ exercise_1/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div>
       <Header course={course['name']} />
-      {/* <Content parts={parts} /> */}
+      <Content parts={course['parts']} />
       <Total parts={course['parts']} />
     </div>
   )
--- exercise_1/src/Total.js
+++ exercise_1/src/Total.js
@@ -1,7 +1,7 @@
-const Total = (parts) => {
+const Total = (props) => {
     return (
       <div>
-        <p>Number of exercises {parts[0]['exercises'] + parts[1]['exercises'] + parts[2]['exercises']}</p>
+        <p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>
       </div>
     )
   }
