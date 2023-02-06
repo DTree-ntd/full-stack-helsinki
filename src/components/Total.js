de650b7bd04e26c821b37ce364e5144035f17916
duc nguyen <ngtrungduc29@gmail.com>
2023-02-06 17:36:21 +0700

    part2-3

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,12 @@
 const Total = (props) => {
-    return (
-      <div>
-        <p>total of {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises'] + props.parts[3]['exercises']} exercises</p>
-      </div>
-    )
-  }
+  const parts = props.parts
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <p>total of {totalExercises} exercises</p>
+    </div>
+  )
+}
   
-  export default Total
+export default Total
