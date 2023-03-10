8aae9cadc4a54c72d11e2a286b0d39b28b573822
duc nguyen <ngtrungduc29@gmail.com>
2023-03-10 17:31:38 +0700

    part2.11 phone book receive data from server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from "../node_modules/axios/index"
 import Filter from "./components/Filter"
 import Person from "./components/Person"
 import PersonForm from "./components/PersonForm"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -60,7 +65,7 @@ const App = () => {
       <PersonForm name={newName} handleName={handleNameChange} number={newNumber} handleNumber={handleNumberChange} handleNewPerson={addPerson}/>
 
       <h2>Numbers</h2>
-      
+
       <Person persons={persons} keyword={keyword}/>
     </div>
   )
